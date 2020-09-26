697cc7fc77693fe7046a8c3e1419e3563068193c
priyanka-teke05 <priyankateke1@gmail.com>
2020-09-26 10:35:47 +0530

    ADDED: assign letter to player and computer

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -8,17 +8,37 @@ class TicTacToeGame {
 		for(int i=1;i<gameBoard.length;i++) {
 			gameBoard[i]='-';
 		}
+	}
+
+	public void displayGameBoard(char gameBoard[]) {
+		for(int i=1;i<gameBoard.length;i=i+3) {
+			System.out.println("--------------");
+         System.out.println("| "+gameBoard[i]+" | "+gameBoard[(i+1)] + " | " + gameBoard[(i+2)]+" | " );
+      }
+		 System.out.println("--------------");
 
 	}
 
 	public static void main(String args[]) {
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
-
+		Scanner sc= new Scanner(System.in);
 		//declare gameboard
 		char[] gameBoard = new char[10];
 
+		char player='X';
+		char computer='O';
+
 		//call method to assign board
 		ticTacToeGame.assignBoard(gameBoard);
+
+		int choosePlayer = 0;
+		if( choosePlayer == 0) {
+			System.out.println("player turn\nEnter cell number from 1 to 9");
+			int cellNumber = sc.nextInt();
+			gameBoard[cellNumber]= player;
+		}
+
+		ticTacToeGame.displayGameBoard(gameBoard);
 	}
 }
 
